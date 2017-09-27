bf12a364d5c8c234b703838d98b2fae272fdc5ab
Justine Cantado <justinecantado@gmail.com>
2017-09-27 05:47:18 +0000

    comment codeblocks

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,12 @@
 "use strict"
+/*
+QUESTIONS PROPERTIES IN ARRAY OBJECTS 
+WHERE IN 'question' property is question 
+WHERE IN 'choices' property is question choices representation
+WHERE IN 'answer' property is question correct answer
+
+*/
+
 var Allquestions = [{
         question: "Who is naruto's father?",
         choices: ["Jiraiya", "Sarutobi", "Orichimaru", "Minato"],
@@ -32,31 +40,33 @@ var Allquestions = [{
         answer: 1
     },
     {
-        question: "Was naruto able to control his jinjuriki during the paint attack?",
+        question: "Was naruto able to control his jinjuriki during the pain attack?",
         choices: ["Yes, his father helped him", "Yes, he trained at Mount Myōboku to control the 9 tails power", "No, he wasn't able to control the 9 tails power", "No, he wasn't able to control the 9 tails but he became friend with kurama"],
         answer: 1
     }]
-var countPage = 1;
-var container = $('.container');
-var questionWrapper = '#question-wrapper';
-var createFieldSet = '<fieldset></fieldset>'
-var alreadyAnswered = false;
-var page = 0;
-var textNode = "";
-var indexController = 0;
+
+// GLOBAL SCOPE //
+var answersArr = []; // STORED ANSWERS //
+var countPage = 1; // FAKE PAGE DEFAULT VALUE //
+var alreadyAnswered = false; // UNIQUE ANSWER VALIDATION //
+var page = 0; // INDEX PAGE DEFAULT VALUE //
+var textNode = ""; // QUESTION TEXT DEFAULT VALUE //
+var indexController = 0; // INDEX DEFAULT VALUE //
+// READY AND CALL //
 $(document).ready(function() {
-    createQuiz(page);
+    createQuiz(page); //CREATE QUESTION DEFAULT // 
     $("#navigate").on('click', function() {
-        page += 1;
-        if (page > 6) page = 0;
-        indexController = page;
-        createQuiz(page);
-        updateDomPage(countPage);
+        page += 1; // INCREMENT OUR PAGE COUNT //
+        if (page > 6) page = 0; // BACK TO 0 ON LIMIT //
+        indexController = page; //INDEX EQUALS TO CURRENT PAGE // 
+        createQuiz(page); // UPDATE QUIZ //
+        updateDomPage(countPage); // LETS UPDATE PAGE COUNT //
     });
 })
 
-
+// FUNCTIONS //
 function createQuiz(crPage) {
+    // CURRENT INDEX QUESTION //
     textNode = Allquestions[indexController].question;
     switch (crPage) {
         case 0:
@@ -94,6 +104,14 @@ function createQuiz(crPage) {
     }
 }
 
+
+
+function validateAnswer(ans){
+    this.answer = ans;
+    
+}
+
+// UPDATE PAGE COUNT //
 function updateDomPage(val) {
     val = countPage += 1;
     var pageHTML = $("#currentPage");
@@ -106,13 +124,17 @@ function updateDomPage(val) {
         pageHTML.html(val);
     }
 }
-
+// APPEND QUESTION CHOICES BASED ON QUESTION INDEX //
 function appendChoices() {
+    $("#choices").empty(); // REMOVE EXISTING CHOICES //
     for (var i = 0; i < Allquestions[indexController].choices.length; i++) {
-            console.log(Allquestions[indexController].choices[i]);
+            console.log(Allquestions[indexController].choices[i]); // TEST ACCESS
+            $("#choices").append("<input type='radio' id='"+ indexController +"' name='answer'>") //CREATE RADIO BUTTON WITH ID BASE ON INDEX//
+            $("#choices").append("<label for='" +indexController + "'>" + Allquestions[indexController].choices[i]+ "</label>") // CREATE LABEL CHOICES REPRESENTATION //
     }
 }
+// APPEND QUESTIONS BASED ON INDEX //
 function appendQuestion(){
-                $('legend').empty()
-            .append(textNode);
+                $('legend').empty() // REMOVE EXISTING TEXTNODES // 
+            .append(textNode); // APPEND CURRENT INDEX TEXTNODES //
 }
